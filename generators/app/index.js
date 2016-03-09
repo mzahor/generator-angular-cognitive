3b4be9013ce1db0bf67833042ee2ebc5e560961b
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2016-03-09 16:18:22 +0200

    clean up app generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,37 +4,20 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+    prompting: function() {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the well-made ' + chalk.red('generator-angular-cognitive') + ' generator!'
-    ));
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the ' + chalk.red('generator-angular-cognitive') +
+            ' generator! Generating app is not currently supported. See --help.'
+        ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        this.prompt(prompts, function(props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
-  },
-
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  },
-
-  install: function () {
-    this.installDependencies();
-  }
+            done();
+        }.bind(this));
+    },
 });
